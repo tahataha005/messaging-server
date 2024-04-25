fd770dac310d23f746fdb5e86da0a7b42d1b65c7
tahataha005 <taha.f.taha0@gmail.com>
2024-04-26 00:43:51 +0300

    Remove <> Build

--- build/core/tools/register.helper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerHelper = void 0;
-const registerHelper = (app, helper) => {
-    app.use(helper());
-};
-exports.registerHelper = registerHelper;
